4da9cea7f9cc8228517d442a4dd1cfe801b3386e
pablo rojas <pablorojas654@gmail.com>
2023-04-05 22:02:46 -0300

    clase 15 metodo equals & hashCode

--- src/com/alura/modelo/Curso.java
+++ src/com/alura/modelo/Curso.java
@@ -1,14 +1,13 @@
 package com.alura.modelo;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Curso {
 
     private String nombre;
     private int tiempo;
     private List<Aula> aulaList = new ArrayList<>();
+    private Collection<Alumno> alumnos = new HashSet<>();
 
     public String getNombre() {
         return nombre;
@@ -44,6 +43,16 @@ public class Curso {
     public void addAula(Aula aula){
         this.aulaList.add(aula);
     }
+
+    public void addAlumno(Alumno alumno){
+        this.alumnos.add(alumno);
+    }
+
+    public Collection<Alumno> getAlumnos() {
+        return alumnos;
+    }
+
+    public boolean verificaAlumno(Alumno alumno){ return this.alumnos.contains(alumno);}
 
     public void setAulaList(List<Aula> aulaList) {
         this.aulaList = aulaList;
